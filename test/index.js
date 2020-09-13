2fbf9f89d373e3f0284d2b6b91cc2666a7ef58ec
choilm <choilmto@gmail.com>
2020-09-13 11:00:04 -0400

    Rewrite tests to meet requirements

--- test/index.js
+++ test/index.js
@@ -4,13 +4,10 @@ const getPort = require("get-port");
 const nock = require("nock");
 const server = require("../server");
 const distributions = require("./distributions");
-const minSecure = require("./minimum-secure");
-const latestReleases = require("./latest-releases");
 
 const getJSON = bent("json");
 const getBuffer = bent("buffer");
 
-// Use `nock` to prevent live calls to remote services
 nock("https://nodejs.org")
   .persist()
   .get("/dist/index.json")
@@ -28,46 +25,28 @@ tape("setup", async function (t) {
 });
 
 tape("should get dependencies", async function (t) {
-  const html = (await getBuffer(`${context.origin}/dependencies`))
-    .toString()
-    .replace(/\s+/g, " ");
+  const html = (await getBuffer(`${context.origin}/dependencies`)).toString();
 
-  const mockDependencies = `<!DOCTYPE html>
-    <html lang="en" dir="ltr">
-
-      <head>
-        <meta charset="utf-8">
-        <title></title>
-      </head>
-
-      <body>
-        <ul>
-          <li>bent - ^7.3.10</li>
-          <li>express - ^4.17.1</li>
-          <li>hbs - ^4.1.1</li>
-          <li>semver - ^7.3.2</li>
-        </ul>
-      </body>
-
-    </html>`.replace(/\s+/g, " ");
-
-  t.plan(1);
-  t.equal(mockDependencies, html);
-  t.end();
+  t.plan(3);
+  t.match(html, /bent/, "should contain bent");
+  t.match(html, /express/, "should contain express");
+  t.match(html, /hbs/, "should contain hbs");
 });
 
 tape("should get minimum secure versions", async function (t) {
   const dists = await getJSON(`${context.origin}/minimum-secure`);
-  t.plan(1);
-  t.deepEqual(dists, minSecure);
-  t.end();
+
+  t.plan(2);
+  t.equal(dists.v0.version, "v0.12.17", "v0 version should match");
+  t.equal(dists.v4.version, "v4.9.0", "v4 version should match");
 });
 
 tape("should get latest version for each major release", async function (t) {
   const dists = await getJSON(`${context.origin}/latest-releases`);
-  t.plan(1);
-  t.deepEqual(dists, latestReleases);
-  t.end();
+
+  t.plan(2);
+  t.equal(dists.v14.version, "v14.10.1", "v14 version should match");
+  t.equal(dists.v13.version, "v13.14.0", "v13 version should match");
 });
 
 tape("teardown", function (t) {
